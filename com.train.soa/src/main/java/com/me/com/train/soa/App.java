64e35abf9fcc26e54f746a7698fd5ac30591e091
sreerama-naga <sreerama.naga@gmail.com>
2017-03-06 10:19:44 +0530

    first commit

---
+++ com.train.soa/src/main/java/com/me/com/train/soa/App.java
@@ -0,0 +1,13 @@
+package com.me.com.train.soa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
